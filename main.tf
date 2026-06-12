0de3e7e782b1524beb1e49513c00ac1f3bdf8877
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2026-06-12 07:34:40 +0530

    Add AWS provider and EC2 instance configuration

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "existing_instance" {
+  ami           = "ami-0db56f446d44f2f09"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "JUMP Svr"
+  }
+}
